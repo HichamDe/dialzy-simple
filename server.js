f1e611e432ede6be055e70bdea70d97dd06f2439
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2024-06-07 15:42:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 const PORT = 5000;
-
+// route
 app.get("/",(req,res)=> res.send("Hello Welcome in Dialzy Azure"));
 
 app.listen(PORT,console.log(`Running at port:${PORT}`));
